095c0424994b81c6f21fa5e73646785d7be2f115
anassmisri2002 <2100914@talnet.com>
2019-11-26 13:23:52 +0100

    gefixt

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,12 +1,13 @@
-functioncheck(){
-var saldo = document.getElementById("saldo").value;
+function check() {
+var saldo =  document.getElementById ("saldo").value;
+
  
 if(saldo==500){
         document.getElementById("tekst").innerHTML ="uw salaris is gestort!"
-}elseif(saldo>500){
+}else if(saldo>500){
             document.getElementById("tekst").innerHTML ="uw salaris met uw bonus is gestort!"
  
-}else{
+} else{
         document.getElementById("tekst").innerHTML ="Er is geen geld gestort!";
 
 }
