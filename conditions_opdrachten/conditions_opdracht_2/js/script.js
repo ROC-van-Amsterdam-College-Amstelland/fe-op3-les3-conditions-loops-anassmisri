3851038236bc59aaea077150767252cefa7e4c9e
anassmisri2002 <2100914@talnet.com>
2019-11-26 13:10:50 +0100

    ja leuk

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,11 @@
-//javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+ 
+    if (saldo<25) {
+        document.getElementById("tekst").innerHTML = "Sorry, je hebt te weinig saldo!"
+ 
+    } else {
+        document.getElementById("tekst").innerHTML = "Je hebt voldoende saldo om € 25 op te nemen";
+    }
+ 
+}
