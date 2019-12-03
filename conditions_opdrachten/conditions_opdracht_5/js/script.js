b44dc05f2a557ed443b6a2770c8e17146279aa6e
anassmisri2002 <2100914@talnet.com>
2019-12-03 13:09:00 +0100

    !

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,11 @@
-//javascript code
+function optellen() {
+    var getal1 = parseInt(doument.getElementById("getal1").value)
+    var getal2 = parseInt(doument.getElementById("getal2").value)
+    document.getElementById("antwoord").innerHTML=getal1+getal2
+}
+
+function aftrekken() {
+    var getal1 = parseInt(doument.getElementById("getal1").value)
+    var getal2 = parseInt(doument.getElementById("getal2").value)
+    document.getElementById("antwoord").innerHTML=getal1-getal2
+ }
