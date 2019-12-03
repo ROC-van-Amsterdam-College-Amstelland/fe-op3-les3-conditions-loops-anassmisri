130858b00adeaae5e83f253bdee67475574bf078
anassmisri2002 <2100914@talnet.com>
2019-12-03 16:14:03 +0100

    opdracht 7

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,11 +1,22 @@
 function optellen() {
     var getal1 = parseInt(doument.getElementById("getal1").value)
     var getal2 = parseInt(doument.getElementById("getal2").value)
-    document.getElementById("antwoord").innerHTML=getal1xgetal2
+    document.getElementById("antwoord").innerHTML=getal1+getal2
 }
 
 function aftrekken() {
     var getal1 = parseInt(doument.getElementById("getal1").value)
     var getal2 = parseInt(doument.getElementById("getal2").value)
+    document.getElementById("antwoord").innerHTML=getal1-getal2
+ }
+ function delen() {
+    var getal1 = parseInt(doument.getElementById("getal1").value)
+    var getal2 = parseInt(doument.getElementById("getal2").value)
     document.getElementById("antwoord").innerHTML=getal1/getal2
+}
+
+function vermenigvuldigen() {
+    var getal1 = parseInt(doument.getElementById("getal1").value)
+    var getal2 = parseInt(doument.getElementById("getal2").value)
+    document.getElementById("antwoord").innerHTML=getal1*getal2
  }
