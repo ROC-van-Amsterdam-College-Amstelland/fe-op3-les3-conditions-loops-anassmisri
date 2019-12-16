8ba0736c0efc4a1f276e16d35b6138e71e01b5cd
anassmisri2002 <2100914@talnet.com>
2019-12-16 15:02:33 +0100

    opd 9

--- loops_opdrachten/loops_opdracht_9/js/script.js
+++ loops_opdrachten/loops_opdracht_9/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+for (let i=0; i<=5; i++) { 
+    task(i); 
+ } 
+   
+ function task(i) { 
+   setTimeout(function() { 
+    if (i == 1) {
+                document.getElementById("uitkomst").innerHTML = "Het Einde";
+            } 
+       
+       // Add tasks to do 
+   }, 2000 * i); 
+ } 
+ document.getElementById("uitkomst").innerHTML = "Het Begin Begint"
