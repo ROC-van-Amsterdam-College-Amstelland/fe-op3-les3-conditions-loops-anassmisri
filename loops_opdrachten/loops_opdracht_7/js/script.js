5e1f36d3ff58a9db1b06d27b7fd95b696988156f
anassmisri2002 <2100914@talnet.com>
2019-12-16 14:56:59 +0100

    opd7

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -1,1 +1,16 @@
 //javascript code
+var tekst = "";
+for (var i = 1; i <= 100; i += 1) {
+
+    tekst += i + "<br>";
+
+    if (i % 3) {
+        tekst += "vet" + "<br>";
+    } 
+    else if (i % 5) {
+        tekst += "cool"  + "<br>";
+    }
+console.log(i)
+}
+
+document.getElementById("uitkomst").innerHTML = tekst;
