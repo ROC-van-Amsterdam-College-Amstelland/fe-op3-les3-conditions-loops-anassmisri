97baa5bbfd0cbf904c4285197277456a89520b5c
anassmisri2002 <2100914@talnet.com>
2019-12-16 14:48:04 +0100

    opd4

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,16 @@
 //javascript code
+var tekst;
+    for (var i = 0; i <= 1000; i += 10) {
+        console.log(i)
+
+        tekst += i + "<br>";
+
+        if (i == 19) {
+            tekst += "";
+        }
+        else if(i==40){
+            tekst += "";
+        }
+    }
+
+    document.getElementById("uitkomst").innerHTML = tekst;
