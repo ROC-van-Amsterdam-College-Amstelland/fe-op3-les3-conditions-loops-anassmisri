37bb06949b3e33f68333b7ea0678901cd273a63e
anassmisri2002 <2100914@talnet.com>
2019-12-16 14:44:49 +0100

    opd3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+var tekst;
+    for (var i = 1; i <= 50; i += 1) {
+
+        tekst += i + "<br>";
+
+        if (i == 19) {
+            tekst += "Dit zijn mooie getallen";
+        }
+        else if(i==40){
+            tekst += "Dit zijn mooie getallen";
+        }
+    }
+
+    document.getElementById("uitkomst").innerHTML = tekst;
