b6bd43b87d0531060ac76b38d24521382a576f42
anassmisri2002 <2100914@talnet.com>
2019-12-16 14:58:56 +0100

    opd 8

--- loops_opdrachten/loops_opdracht_8/js/script.js
+++ loops_opdrachten/loops_opdracht_8/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+var tekst = "";
+for (var i = 1; i <= 100; i += 1) {
+
+  tekst += i + "<br>";
+
+  if (i == 25) {
+    parseInt(alert("Het getal is 25 BULLSEYE"));
+  }
+
+  console.log(i);
+}
+
+document.getElementById("uitkomst").innerHTML = tekst;
